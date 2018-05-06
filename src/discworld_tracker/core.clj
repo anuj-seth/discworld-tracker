83bdb56e36ee17104773e5e22c4e91bb184233b8
Anuj Seth <ANUJS@amdocs.com>
2018-05-06 22:17:12 +0530

    added rendered for a singel table

--- src/discworld_tracker/core.clj
+++ src/discworld_tracker/core.clj
@@ -1,15 +1,8 @@
 (ns discworld-tracker.core
   (:require [discworld-tracker.tracker-ui :as tracker-ui])
-  (:gen-class
-   :extends javafx.application.Application))
-
-(defn -start
-  [app stage]
-  (tracker-ui/start))
+  (:gen-class))
 
 (defn -main
   "A personal read/not-read tracker for Terry Pratchett's Discworld series of novels"
   [& args]
-  ;; passing the name of our class as the first argument to launch is mandatory.
-  ;; if not passed, javafx gives an error that core/-main is not a subclass of javafx.application.Application
-  (javafx.application.Application/launch discworld_tracker.core (into-array String args)))
+  (tracker-ui/start))
--- src/discworld_tracker/tracker_ui.clj
+++ src/discworld_tracker/tracker_ui.clj
@@ -1,20 +1,47 @@
 (ns discworld-tracker.tracker-ui
   (:require [fn-fx.fx-dom :as dom]
             [fn-fx.controls :as ui]
-            [fn-fx.diff :refer [component defui render should-update?]]))
+            [fn-fx.diff :refer [component defui render should-update?]])
+  (:import (javafx.beans.property ReadOnlyObjectWrapper)))
+
+(defn cell-value-factory
+  [f]
+  (reify javafx.util.Callback
+    (call [this entity]
+      (ReadOnlyObjectWrapper. (f (.getValue entity))))))
+
+(defui TableColumn
+  (render
+   [this {:keys [key name]}]
+   (ui/table-column
+    :text name
+    :cell-value-factory (cell-value-factory #(key %)))))
+
+(defui BookView
+  (render [this books]
+          (ui/table-view
+           :items books
+           :columns (map (fn [key header]
+                           (table-column {:key key
+                                          :name header}))
+                         [:volume-number :title :year-published :subseries :notes]
+                         ["#" "Title" "Publication Year" "Subseries" "Notes"]))))
+
+(defui Stage
+  (render [this args]
+          (ui/stage
+           :title "Hello World!"
+           :shown true
+           :scene (ui/scene
+                   :root (book-view args)
+                   ))))
 
 (defn start
   []
-  (let [u (ui/stage
-           :title "Hello World!"
-           :shown true
-           :min-width 300
-           :min-height 300
-           :scene (ui/scene
-                   :root (ui/stack-pane
-                          :children [(ui/button
-                                      :text "Say 'Hello World'"
-                                      :on-action {:say "Hello World!"})])))
-        handler-fn (fn [evt]
-                     (println "Received Event: " evt))]
-    (dom/app u handler-fn)))
+  (let [data-state (atom (clojure.edn/read-string (slurp (clojure.java.io/resource "books.edn"))))
+        handler-fn (fn [args])
+        ui-state (agent (dom/app (stage @data-state) handler-fn))]
+    (add-watch data-state :ui (fn [_ _ _]
+                                (send ui-state
+                                      (fn [old-ui]
+                                        (dom/update-app old-ui (stage @data-state))))))))
